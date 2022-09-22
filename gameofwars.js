df220cd7ac0fb58b281ef7ef74ffa68bb265042a
Lin Lin <linlin@lins-air.myfiosgateway.com>
2022-09-22 19:50:35 -0400

    game logic

--- gameofwars.js
+++ gameofwars.js
@@ -34,3 +34,84 @@ class Deck {
     return this.cards;
   }
 }
+
+class GameOfWar {
+  constructor() {
+    this.playerOne = []
+    this.playerTwo = []
+    this.drawnPile = []
+    this.round = 0
+  }
+  gameSetup() {
+    const deck = new Deck()
+    let playDeck = deck
+    playDeck.shuffle() //shuffles gameplay deck
+    //split deck into two
+    this.playerOne.push(...playDeck.cards.splice(0, playDeck.cards.length / 2))
+    this.playerTwo.push(...playDeck.cards.splice(0, playDeck.cards.length))
+  }
+  war() {
+    this.round++
+    console.log(`Round: ${this.round }\n WAR!`)
+    console.log(`Player one's hand: ${this.drawnPile[0].rank} of ${this.drawnPile[0].suit}\n Player two's hand: ${this.drawnPile[1].rank} of ${this.drawnPile[1].suit}`)
+    let playerOneWar = (this.playerOne.splice(0,4))
+    console.log(`Player one war hand:`)
+    console.log(playerOneWar)
+    let playerTwoWar = (this.playerTwo.splice(0,4))
+    console.log(`Player two war hand:`)
+    console.log(playerTwoWar)
+    
+    this.drawnPile.push(...playerOneWar)
+    this.drawnPile.push(...playerTwoWar)
+    if (this.drawnPile[this.drawnPile.length - 5].score > this.drawnPile[this.drawnPile.length - 1].score) {
+      
+      console.log(`Round: ${this.round}\n Player one's hand: ${this.drawnPile[this.drawnPile.length - 5].rank} of ${this.drawnPile[this.drawnPile.length - 5].suit}\n Player two's hand: ${this.drawnPile[this.drawnPile.length - 1].rank} of ${this.drawnPile[this.drawnPile.length - 1].suit}\n Player ONE WINS WAR!`)
+      this.playerOne.push(...this.drawnPile)
+      this.drawnPile = [];
+    } else if (this.drawnPile[this.drawnPile.length - 5].score < this.drawnPile[this.drawnPile.length - 1].score) {
+     
+      console.log(`Round: ${this.round}\n Player one's hand: ${this.drawnPile[this.drawnPile.length - 5].rank} of ${this.drawnPile[this.drawnPile.length - 5].suit}\n Player two's hand: ${this.drawnPile[this.drawnPile.length - 1].rank} of ${this.drawnPile[this.drawnPile.length - 1].suit}\n Player TWO WINS WAR!`)
+      this.playerTwo.push(...this.drawnPile)
+      this.drawnPile = [];
+    } else {
+      this.war();
+    }
+  }
+
+  gamePlay() {
+
+    while (this.playerOne.length !== 0 && this.playerTwo.length !== 0) {
+      this.drawnPile.push(this.playerOne.pop())
+      this.drawnPile.push(this.playerTwo.pop())
+
+      if (this.drawnPile[0].score === this.drawnPile[1].score) {
+        this.war();
+      }
+
+      else if (this.drawnPile[0].score > this.drawnPile[1].score) {
+        this.round++
+        console.log(`Round: ${this.round}\n Player one's hand: ${this.drawnPile[0].rank} of ${this.drawnPile[0].suit}\n Player two's hand: ${this.drawnPile[1].rank} of ${this.drawnPile[1].suit}\n Player one wins`)
+        this.playerOne.push(...this.drawnPile)
+        this.drawnPile = [];
+      }
+
+      else if (this.drawnPile[0].score < this.drawnPile[1].score) {
+        this.round++
+        console.log(`Round: ${this.round}\n Player one's hand: ${this.drawnPile[0].rank} of ${this.drawnPile[0].suit}\n Player two's hand: ${this.drawnPile[1].rank} of ${this.drawnPile[1].suit}\n Player Two wins`)
+      this.playerTwo.push(...this.drawnPile)
+      this.drawnPile = [];
+    }
+
+  }
+  if(this.playerOne.length === 52) {
+  console.log(`Player One is the ULTIMATE winner with ${this.playerOne.length} cards on round ${this.round} `)
+} 
+        else if (this.playerTwo.length === 52) {
+  console.log(`Player Two is the ULTIMATE winner with ${this.playerTwo.length} cards on round ${this.round}`)
+} 
+      } 
+}
+
+let gameOne = new GameOfWar()
+gameOne.gameSetup()
+gameOne.gamePlay()
